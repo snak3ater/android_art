8227e938e9cbd364b13c0fbdc272dcdab8c25ab8
Sebastien Hertz <shertz@google.com>
2014-03-14 17:58:54 +0100

    Fix reg type merging in the verifier
    Fixes bad merging of float, long and double reg types with constant types. Adds
    merging tests in reg_type_test.
    
    Bug: https://code.google.com/p/android/issues/detail?id=66434
    Bug: 13464407
    Change-Id: I56761f5f5562b742d0ef9cc66b58d55f7b668949
    
    Conflicts:
    	runtime/verifier/reg_type_test.cc

--- runtime/verifier/method_verifier.cc
+++ runtime/verifier/method_verifier.cc
@@ -4159,6 +4159,7 @@ const MethodReference* MethodVerifier::GetDevirtMap(const MethodReference& ref,
 
 std::vector<int32_t> MethodVerifier::DescribeVRegs(uint32_t dex_pc) {
   RegisterLine* line = reg_table_.GetLine(dex_pc);
+  DCHECK(line != nullptr) << "No register line at DEX pc " << StringPrintf("0x%x", dex_pc);
   std::vector<int32_t> result;
   for (size_t i = 0; i < line->NumRegs(); ++i) {
     const RegType& type = line->GetRegisterType(i);
